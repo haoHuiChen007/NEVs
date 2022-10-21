b6fbcaf7d851dee29ae11d964927a91a0adbfe94
陈浩辉 <2954390791@qq.cmo>
2022-10-21 14:13:23 +0800

    收藏夹实体

--- src/main/java/com/example/nevs/module/car/entity/Car.java
+++ src/main/java/com/example/nevs/module/car/entity/Car.java
@@ -1,5 +1,14 @@
 package com.example.nevs.module.car.entity;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
 public class Car {
+    private String id;
+    private Integer carType;
+    private String carName;
+    private String brand;
+    private Double price;
+    @DBRef
+    private CarParams carParams;
 
 }
---
+++ src/main/java/com/example/nevs/module/car/entity/CarParams.java
@@ -0,0 +1,11 @@
+package com.example.nevs.module.car.entity;
+
+public class CarParams {
+    private String id;
+    private String color;
+    private String engine;
+    private String fuelConsumption;
+    private String gearbox;
+    private String horsepower;
+    private String maxSpeed;
+}
